d46380a4ccc7946624ee6a02a704f5f2cf8172a3
xzpeng <pengxiaozhou2008@gmail.com>
2017-04-20 11:42:20 +0800

    修改mysql配置，使用aws

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+if (!defined('RDS_HOSTNAME')) {
+  define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
+  define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
+  define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
+  define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
+}
+
 return [
 
     /*
@@ -54,11 +61,18 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
+            
+            'host' => RDS_HOSTNAME,
+            'database' => RDS_DB_NAME,
+            'username' => RDS_USERNAME,
+            'password' => RDS_PASSWORD,
+
+            /*'host' => env('DB_HOST', 'localhost'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', ''),*/
+
+            'port' => env('DB_PORT', '3306'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
